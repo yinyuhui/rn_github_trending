8db52095891c36a1657afe3f1e457a4b2e80b7fb
尹玉慧 <veinyin@gmail.com>
2020-08-13 20:03:38 +0800

    自定义 logger 中间件

--- js/store/index.js
+++ js/store/index.js
@@ -2,5 +2,17 @@ import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducer'
 
-const middleware = [thunk]
+// 自定义中间件
+const logger = (store) => (next) => (action) => {
+    if (typeof action === 'function') {
+        console.log('action is a function')
+    } else {
+        console.log('action - ', action)
+    }
+    const result = next(action)
+    console.log('nextState - ', store.getState())
+    return result
+}
+
+const middleware = [logger, thunk]
 export default createStore(reducers, applyMiddleware(...middleware))
